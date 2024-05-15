e53d6c2404f1d29e9beb013ffec9147dedb7da65
Lenghak Hok <lenghakhok.dev@gmail.com>
2024-05-15 11:12:28 +0700

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { csrf } from "hono/csrf";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+import { secureHeaders } from "hono/secure-headers";
+import { trimTrailingSlash } from "hono/trailing-slash";
+const app = new Hono();
+
+// util
+app
+	.use(cors())
+	.use(csrf())
+	.use(logger())
+	.use(prettyJSON())
+	.use(secureHeaders())
+	.use(trimTrailingSlash());
+
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
+
+export default {
+	port: 9000,
+	fetch: app.fetch,
+};
